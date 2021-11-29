e577821b084ea7648fe55930427e6a046230aa3a
Zapa120 <Zapaabuhay120@icloud.com>
2021-11-29 17:55:51 +0200

    uinitest

---
+++ dbcontrol_test.py
@@ -0,0 +1,20 @@
+import unittest
+import dbcontrol
+from dbcontrol import *
+
+class dbcontrol(unittest.TestCase):
+    def test_Search(self):
+        self.assertTrue(Search('OrB'), "Should be True")
+        self.assertFalse(Search('222'), "Should be False")
+        self.assertFalse(Search('Barak4B'), "Should be False")
+
+
+
+
+
+
+
+
+
+    if __name__ == '__main__':
+        unittest.main()
