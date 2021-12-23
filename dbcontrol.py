f1872e4095822b9d3b687176bed3550632a0eb95
OrBonker <94792373+OrBonker@users.noreply.github.com>
2021-12-23 11:31:10 +0200

    update

--- dbcontrol.py
+++ dbcontrol.py
@@ -25,41 +25,9 @@ def newcustomer(userName,Password,firstName,lastName,Address,phoneNumber,mailAdd
         c.execute("INSERT INTO `Users` ('UserName','Password', 'FirstName', 'LastName', 'Address', 'PhoneNumber', 'MailAddress', 'UserType') VALUES (?,?,?,?,?,?,?,?);",(userName,Password,firstName,lastName,Address,phoneNumber,mailAddress,userType))
         conn.commit()
 
-def printUser(userName):
-    # A function that prints the user information you requested
-    c.execute('SELECT * FROM `Users` WHERE UserName ="' + userName + '" ' )
-    for row in c:
-        print(row)
-
 def newAnimal(userID,Type,animalName,importantInfo):
     # A function that creates an animal for the user according to ID
     c.execute("INSERT INTO `Animals` ('userID','Type','animalName','importantInfo') VALUES (?,?,?,?);",(userID,Type,animalName,importantInfo))
-
-def printanimals(UserID):
-    # A function that prints all the animals of the user according to ID
-    c.execute('SELECT * FROM `Animals` WHERE UserID ="' + UserID + '" ' )
-    for row in c:
-        print(row)
-
-# print("plz enter your customer details:")
-# UserName=input("User Name")
-# Password=input("Password,Letters and Numbers")
-# FirstName=input("First Name")
-# LastName=input("Last Name")
-# Address=input("Hometown")
-# PhoneNumber=input("Phone Number")
-# MailAddress=input("Mail Address")
-# UserType=input("User Type")
-#
-# newcustomer(UserName,Password,FirstName,LastName,Address,PhoneNumber,MailAddress, UserType)
-# printUser(UserName)
-# print("plz enter the animal details:")
-# UserID=input("customer User ID")
-# Type= input("User Type of animal")
-# AnimalName=input("Animal Name")
-# ImportantInfo=input("Important Info about the animal")
-# newAnimal(UserID,Type,AnimalName,ImportantInfo)
-# printanimals(UserID)
 
 # Gets a username and password , and checks if it exists in the system - If so returns UserID
 def Login_check (Name , Password):
@@ -171,12 +139,6 @@ def Show_appointment(Date):
 conn.commit()
 # conn.close()
 
-#print(Show_appointment('05/10/21'))
-#print_appoin('05/10/21')
-#print(Show_appointment_today('30/10/21'))
-#t=AnimalName(5)
-#print(t)
-
 #A function that gets a UserID and returns the username
 def UserName(ID):
     c.execute("SELECT * FROM Users WHERE UserID=?",(ID,))
@@ -188,5 +150,4 @@ def UserName(ID):
         return t
 
 
-#z=UserName(5)
-#print(z)
+
